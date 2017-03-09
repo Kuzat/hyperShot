2f3c533a0d9080e3ccc129a3aa4f3cee18ec25b5
Vegard Bjørsvik <vegardbjr@gmail.com>
2017-03-09 01:19:06 -0800

    New native addon for taking screenshot on linux/ unix

--- renderer/snippet.js
+++ renderer/snippet.js
@@ -7,6 +7,7 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log("here");
     let startPos = {x: 0, y: 0};
     let mouseDown = false;
+    let x1, y1, x2, y2;
     const canvas = document.getElementById('canvas');
     const context = canvas.getContext("2d");
 
@@ -26,30 +27,31 @@ document.addEventListener('DOMContentLoaded', () => {
 
     document.onmousemove = function(event) {
         console.log(event);
-        // Calcualte the width and height
-        let x1 = Math.min(startPos.x, event.screenX);
-        let y1 = Math.min(startPos.y, event.screenY);
-        let x2 = Math.max(startPos.x, event.screenX);
-        let y2 = Math.max(startPos.y, event.screenY);
+
 
 
         //check if we have had a mouse down event.
         if(mouseDown) {
+            // Fill old rectangle
+            context.fillRect(x1, y1, x2 - x1, y2 - y1);
+
+            // Calcualte the width and height
+            x1 = Math.min(startPos.x, event.screenX);
+            y1 = Math.min(startPos.y, event.screenY);
+            x2 = Math.max(startPos.x, event.screenX);
+            y2 = Math.max(startPos.y, event.screenY);
+
             // remove background that is selected
-            context.clearRect(x1, y1, x2, y2);
-            // Clear and fill the rest to update it. 4 rects; Above, bellow, and both sides.
-            //clear
-            context.clearRect(0, 0, canvas.width, startPos.y);
-            context.clearRect(0, event.screenY, canvas.width, canvas.height-event.screenY);
-            context.clearRect(0, startPos.y, startPos.x, event.screenY-startPos.y);
-            context.clearRect(event.screenX, startPos.y, canvas.width-event.screenX, event.screenY-startPos.y);
-            //fill
-            //context.fillStyle = "rgba(0, 0, 0, 0.5)";
-            context.fillRect(0, 0, canvas.width, startPos.y);
-            context.fillRect(0, event.screenY, canvas.width, canvas.height-event.screenY);
-            context.fillRect(0, startPos.y, startPos.x, event.screenY-startPos.y);
-            context.fillRect(event.screenX, startPos.y, canvas.width-event.screenX, event.screenY-startPos.y);
+            context.clearRect(x1, y1, x2- x1, y2 - y1);
         }
+    }
+
+    document.onmouseup = function(event) {
+        console.log(event);
+
+        // mouse no longer down
+        mouseDown = false;
+
     }
 });
 
